fc5670cc292df48d8918f116bce3ded440013cb6
Giacomo Nanni <gn.nanni@gmail.com>
2025-03-06 12:05:41 +0100

    project setup

---
+++ src/routes/home/[page]/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	export let data;
+	console.log(data);
+</script>
+
+<div>
+	{#if data.posts.status == "error"}
+		<p>404</p>
+	{:else}
+		<p>{data.posts.result.title}</p>
+	{/if}
+</div>
